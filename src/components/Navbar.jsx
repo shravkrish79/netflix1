0bcbb0b2a1f7ebc2d3572fded43d854b5f22e3e1
shravkrish79 <shravkrish79@gmail.com>
2023-04-14 01:56:50 +0530

    search function added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,17 @@ import userIcon from "../assets/netflix-profile-icons/216 - 6FgZxbi.png";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../state/useUser";
+import { useCategory } from "../state/useCategory";
 import "../scripts/scroll";
+import { useEpisode } from "../state/useEpisode";
+
 
 export default function Navbar() {
     const Navigate = useNavigate();
     const { saveUID, setUid, setIsAdmin, saveAdmin } = useUser();
     const [toggle, setToggle] = useState(false);
+    const { displayData, setDisplayData, initDisplayData } = useCategory();
+    const { setIsSearch } = useEpisode();
 
     function showsearchbox() {
         setToggle(!toggle);
@@ -24,7 +29,30 @@ export default function Navbar() {
         setIsAdmin(false);
         Navigate("/");
     }
-
+    // console.log(displayData)
+    function searchVideo(e) {
+        e.preventDefault();
+        setIsSearch({ type: 'SET_ITEM', payload: true });
+        const searchkey = e.target.value;
+        const searchTerm = searchkey.toLowerCase();
+        if (searchkey !== '') {
+            const filterData = displayData.map((recs) => {
+                return { ...recs, dataList: recs.dataList.filter((itm) => itm.Title.toLowerCase().includes(searchTerm)) }
+            });
+            let dataLength = 0;
+            filterData.map((recs) => { dataLength += recs.dataList.length });
+            if (dataLength > 0) {
+                setDisplayData({ type: 'SET_ITEM', payload: filterData });
+            }
+            else {
+                setDisplayData({ type: 'SET_ITEM', payload: initDisplayData });
+            }
+        }
+        else {
+            setDisplayData({ type: 'SET_ITEM', payload: initDisplayData });
+            setIsSearch({ type: 'SET_ITEM', payload: false });
+        }
+    }
 
 
     return (
@@ -40,7 +68,8 @@ export default function Navbar() {
                     <a href="/documentaries">Documentary</a>
                 </div>
                 <div className="searchbox">
-                    <input type="search" id="searchinput" placeholder="Titles, Genres" className={toggle ? "showsearch" : "hidesearch"} />
+                    <input type="search" id="searchinput" placeholder="Titles, Genres" className={toggle ? "showsearch" : "hidesearch"}
+                        onChange={(e) => searchVideo(e)} />
                     <div className={toggle ? "move-search-btn" : "search-btn"} onClick={() => showsearchbox()}><GoSearch id="searchicon" /></div>
                 </div>
                 <a href="javascript:void(0);"><MdOutlineNotificationsNone className="bell-icon" /></a>
